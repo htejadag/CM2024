464b30abc5b57e4e81ac5e953aa3b024afd09614
htejadag <htejadag25@gmail.com>
2024-05-13 21:54:36 -0500

    Implementar boton regresar

--- app/src/main/java/com/example/myappcm2024/MainActivity.java
+++ app/src/main/java/com/example/myappcm2024/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myappcm2024;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +23,12 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void Basicos(View view)
+    {
+        Intent i = new Intent(this, ControlesBasicos.class);
+        startActivity(i);
+        finish();
+    }
+
 }
